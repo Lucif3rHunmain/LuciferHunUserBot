c909315f6c0d72f596357266234977bb625e177d
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-29 14:18:11 +0530

    Update hackerdp.py

--- userbot/plugins/hackerdp.py
+++ userbot/plugins/hackerdp.py
@@ -46,10 +46,20 @@ async def hackpp():
 @borg.on(admin_cmd(pattern="hacker ?(.*)"))
 
 async def main(event):
-
+    input_str = "".join(event.text.split(maxsplit=1)[1:])
+    input_Delay = int(input_str.split(" ", 2)[0])
+    input_Time = str(input_str.split(" ", 2)[1])
+    if input_Time == 'd' or input_Time == 'D':
+     input1_Delay=(input_Delay*24*60*60)
+    elif input_Time == 'h' or input_Time == 'H':
+     input1_Delay=(input_Delay*60*60)
+    elif input_Time == 'm' or input_Time == 'M':
+     input1_Delay=(input_Delay*60)
+    elif input_Time == 's' or input_Time == 'S':
+     input1_Delay=input_Delay
+    else:
+      input1_Delay = 300
     await event.edit("**Starting Hacker Profile Pic...\n\nDone !!! Check Your DP")
-    await asyncio.sleep(4)
-    await event.delete()
     while True:
 
         await hackpp()
@@ -60,6 +70,13 @@ async def main(event):
 
         os.system("rm -rf donottouch.jpg")
 
-        await asyncio.sleep(300)
+        await asyncio.sleep(input1_Delay)
 
-CMD_HELP.update({"hackerdp": ".hackerdp\nUse - Auto-changing dp of Hacker."})
+CMD_HELP.update(
+  {
+    "gamersdp": "`.hackerdp`\n"
+    "Usage - Auto-changing dp of Gamers Every 5 Mins(Default)."
+    "`.hackerdp <delay> <s or S for seconds, m or M for minute(s) Delay, h or H for hour(s) Delay and d or D for Day(s) Delay>`\n"
+    "Usage - Auto-changing dp of Gamers with custom Delay."
+  }
+)
