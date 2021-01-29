a5548b474c2e121056f56a1b98e5db2acfa56de7
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-29 09:28:28 +0530

    Create hackerdp.py

---
+++ userbot/plugins/hackerdp.py
@@ -0,0 +1,64 @@
+import requests , re , random 
+
+import urllib , os 
+
+from telethon.tl import functions
+
+from datetime import datetime
+
+from PIL import Image, ImageDraw, ImageFont
+
+from userbot.utils import admin_cmd
+
+import asyncio
+
+from time import sleep
+
+COLLECTION_STRINGZ = [
+  "hacker-background"
+]
+
+async def hackpp():
+
+    os.system("rm -rf donot.jpg")
+
+    rnd = random.randint(0, len(COLLECTION_STRINGZ) - 1)
+
+    pack = COLLECTION_STRINGZ[rnd]
+
+    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
+
+    f = re.compile('/\w+/full.+.jpg')
+
+    f = f.findall(pc)
+
+    fy = "http://getwallpapers.com"+random.choice(f)
+
+    print(fy)
+
+    if not os.path.exists("f.ttf"):
+
+        urllib.request.urlretrieve("https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf","f.ttf")
+
+    urllib.request.urlretrieve(fy,"donottouch.jpg")
+
+@borg.on(admin_cmd(pattern="hacker ?(.*)"))
+
+async def main(event):
+
+    await event.edit("**Starting Hacker Profile Pic...\n\nDone !!! Check Your DP")
+    await asyncio.sleep(4)
+    await event.delete()
+    while True:
+
+        await hackpp()
+
+        file = await event.client.upload_file("donottouch.jpg")  
+
+        await event.client(functions.photos.UploadProfilePhotoRequest( file))
+
+        os.system("rm -rf donottouch.jpg")
+
+        await asyncio.sleep(300)
+
+CMD_HELP.update({"avengersdp": ".avengersdp\nUse - Auto-changing dp of Hccker."})
