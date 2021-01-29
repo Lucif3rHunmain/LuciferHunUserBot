9ed38593f2cdfc3b424cd9ec12caa48df1e8fdbc
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-29 14:14:12 +0530

    Update spacedp.py

--- userbot/plugins/spacedp.py
+++ userbot/plugins/spacedp.py
@@ -52,7 +52,19 @@ async def spacepp():
 @borg.on(admin_cmd(pattern="spacedp ?(.*)"))
 
 async def main(event):
-
+    input_str = "".join(event.text.split(maxsplit=1)[1:])
+    input_Delay = int(input_str.split(" ", 2)[0])
+    input_Time = str(input_str.split(" ", 2)[1])
+    if input_Time == 'd' or input_Time == 'D':
+     input1_Delay=(input_Delay*24*60*60)
+    elif input_Time == 'h' or input_Time == 'H':
+     input1_Delay=(input_Delay*60*60)
+    elif input_Time == 'm' or input_Time == 'M':
+     input1_Delay=(input_Delay*60)
+    elif input_Time == 's' or input_Time == 'S':
+     input1_Delay=input_Delay
+    else:
+      input1_Delay = 300
     await event.edit("**Starting Space Profile Pic...\n\nDone !!! Check Your DP") #Owner MarioDevs
     while True:
 
