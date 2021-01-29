ec9a2926e75d21da95392328b1ee8ea2e3d64e57
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-29 14:24:29 +0530

    Update and rename beautifullgirlsdp.py to beautifulgirlsdp.py

--- userbot/plugins/beautifulgirlsdp.py
+++ userbot/plugins/beautifulgirlsdp.py
@@ -75,7 +75,19 @@ async def girlspp():
 @borg.on(admin_cmd(pattern="girlsdp ?(.*)"))
 
 async def main(event):
-
+    input_str = "".join(event.text.split(maxsplit=1)[1:])
+    input_Delay = int(input_str.split(" ", 2)[0])
+    input_Time = str(input_str.split(" ", 2)[1])
+    if input_Time == 'd' or input_Time == 'D':
+     input1_Delay=(input_Delay*24*60*60)
+    elif input_Time == 'h' or input_Time == 'H':
+     input1_Delay=(input_Delay*60*60)
+    elif input_Time == 'm' or input_Time == 'M':
+     input1_Delay=(input_Delay*60)
+    elif input_Time == 's' or input_Time == 'S':
+     input1_Delay=input_Delay
+    else:
+      input1_Delay = 300
     await event.edit("**Starting Girls Profile Pic...\n\nDone !!! Check Your DP **")
     while True:
 
@@ -87,6 +99,13 @@ async def main(event):
 
         os.system("rm -rf donottouch.jpg")
 
-        await asyncio.sleep(300)
+        await asyncio.sleep(input1_Delay)
         
-CMD_HELP.update({"beautifullgirlsdp": ".girlsdp\nUse - Auto-changing dp of Beautiful Girls."})
+CMD_HELP.update(
+  {
+    "Beutiful Girls DP": "`.girlsdp`"
+    "\nUsage - Auto-changing dp of Beutiful Girls Every 5 Mins(Default). \n\n"
+    "`.girlsdp` <delay> <s or S for seconds, m or M for minute(s) Delay, h or H for hour(s) Delay and d or D for Day(s) Delay>"
+    "\nUsage - Auto-changing dp of Beautiful Girls with custom Delay. \n\n"
+  }
+)
