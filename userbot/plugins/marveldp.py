4ba6105af43c3692e6532d017de70415dfe5fd78
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-30 16:46:50 +0530

    Update marveldp.py

--- userbot/plugins/marveldp.py
+++ userbot/plugins/marveldp.py
@@ -98,32 +98,37 @@ async def marvelpp():
 @borg.on(admin_cmd(pattern="marveldp ?(.*)"))
 
 async def main(event):
-    input_str = "".join(event.text.split(maxsplit=1)[1:])
-    input_Delay = int(input_str.split(" ", 2)[0])
-    input_Time = str(input_str.split(" ", 2)[1])
-    if input_Time == 'd' or input_Time == 'D':
-     input1_Delay=(input_Delay*24*60*60)
-    elif input_Time == 'h' or input_Time == 'H':
-     input1_Delay=(input_Delay*60*60)
-    elif input_Time == 'm' or input_Time == 'M':
-     input1_Delay=(input_Delay*60)
-    elif input_Time == 's' or input_Time == 'S':
-     input1_Delay=input_Delay
+     input_str = "".join(event.text.split(maxsplit=1)[1:])
+    if input_str == "":
+     input1_Delay = 300
     else:
+     input_Delay = int(input_str.split(" ", 2)[0])
+     input_Time = str(input_str.split(" ", 2)[1])
+     if input_Time == 'd' or input_Time == 'D':
+      input1_Delay=(input_Delay*24*60*60)
+     elif input_Time == 'h' or input_Time == 'H':
+      input1_Delay=(input_Delay*60*60)
+     elif input_Time == 'm' or input_Time == 'M':
+      input1_Delay=(input_Delay*60)
+     elif input_Time == 's' or input_Time == 'S':
+      input1_Delay=input_Delay
+     else:
       input1_Delay = 300
-    await event.edit("**Starting Marvel Profile Pic's...\n\nDone !!! Check Your DP.**")
+
+    await event.edit("**Starting Marvel Profile Pics... **")
     while True:
 
-        await marvelpp()
+        await actresspp()
 
         file = await event.client.upload_file("donottouch.jpg")  
 
         await event.client(functions.photos.UploadProfilePhotoRequest( file))
 
         os.system("rm -rf donottouch.jpg")
-
+        await event.edit("**Done !!! Check Your DP**")
+        await asyncio.sleep(3)
+        await event.delete()
         await asyncio.sleep(input1_Delay)
-
 CMD_HELP.update(
   {
     "Marvel DP": "`.marveldp`"
