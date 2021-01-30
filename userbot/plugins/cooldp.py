38e9c81e49c6597556855c3775d6439d04f4fa6b
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-30 19:43:08 +0530

    Update cooldp.py

--- userbot/plugins/cooldp.py
+++ userbot/plugins/cooldp.py
@@ -78,29 +78,35 @@ async def girlspp():
 
 async def main(event):
     input_str = "".join(event.text.split(maxsplit=1)[1:])
-    input_Delay = int(input_str.split(" ", 2)[0])
-    input_Time = str(input_str.split(" ", 2)[1])
-    if input_Time == 'd' or input_Time == 'D':
-     input1_Delay=(input_Delay*24*60*60)
-    elif input_Time == 'h' or input_Time == 'H':
-     input1_Delay=(input_Delay*60*60)
-    elif input_Time == 'm' or input_Time == 'M':
-     input1_Delay=(input_Delay*60)
-    elif input_Time == 's' or input_Time == 'S':
-     input1_Delay=input_Delay
+    if input_str == "":
+     input1_Delay = 300
     else:
+     input_Delay = int(input_str.split(" ", 2)[0])
+     input_Time = str(input_str.split(" ", 2)[1])
+     if input_Time == 'd' or input_Time == 'D':
+      input1_Delay=(input_Delay*24*60*60)
+     elif input_Time == 'h' or input_Time == 'H':
+      input1_Delay=(input_Delay*60*60)
+     elif input_Time == 'm' or input_Time == 'M':
+      input1_Delay=(input_Delay*60)
+     elif input_Time == 's' or input_Time == 'S':
+      input1_Delay=input_Delay
+     else:
       input1_Delay = 300
-    await event.edit("**Starting Cool Profile Pic...\n\nDone !!! Check Your DP **")
+
+    await event.edit("**Starting Anime Profile Pics... **")
     while True:
 
-        await girlspp()
+        await coolpp()
 
         file = await event.client.upload_file("donottouch.jpg")  
 
         await event.client(functions.photos.UploadProfilePhotoRequest( file))
 
         os.system("rm -rf donottouch.jpg")
-
+        await event.edit("**Done !!! Check Your DP**")
+        await asyncio.sleep(3)
+        await event.delete()
         await asyncio.sleep(input1_Delay)
         
 CMD_HELP.update(
