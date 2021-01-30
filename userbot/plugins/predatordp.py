17164e9ad7ae40d4ba40dcf07b11731e044d1615
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-30 16:48:12 +0530

    Update predatordp.py

--- userbot/plugins/predatordp.py
+++ userbot/plugins/predatordp.py
@@ -29,25 +29,35 @@ async def ppp():
 @borg.on(admin_cmd(pattern="predatordp ?(.*)"))
 async def main(event):
     input_str = "".join(event.text.split(maxsplit=1)[1:])
-    input_Delay = int(input_str.split(" ", 2)[0])
-    input_Time = str(input_str.split(" ", 2)[1])
-    if input_Time == 'd' or input_Time == 'D':
-     input1_Delay=(input_Delay*24*60*60)
-    elif input_Time == 'h' or input_Time == 'H':
-     input1_Delay=(input_Delay*60*60)
-    elif input_Time == 'm' or input_Time == 'M':
-     input1_Delay=(input_Delay*60)
-    elif input_Time == 's' or input_Time == 'S':
-     input1_Delay=input_Delay
+    if input_str == "":
+     input1_Delay = 300
     else:
+     input_Delay = int(input_str.split(" ", 2)[0])
+     input_Time = str(input_str.split(" ", 2)[1])
+     if input_Time == 'd' or input_Time == 'D':
+      input1_Delay=(input_Delay*24*60*60)
+     elif input_Time == 'h' or input_Time == 'H':
+      input1_Delay=(input_Delay*60*60)
+     elif input_Time == 'm' or input_Time == 'M':
+      input1_Delay=(input_Delay*60)
+     elif input_Time == 's' or input_Time == 'S':
+      input1_Delay=input_Delay
+     else:
       input1_Delay = 300
-    await event.edit("**Starting predator Profile Pic.**") #Owner @NihiNivi
+
+    await event.edit("**Starting Predator Profile Pics... **")
     while True:
-        await ppp()
+
+        await actresspp()
+
         file = await event.client.upload_file("donottouch.jpg")  
-        await event.client(functions.photos.DeletePhotosRequest(await event.client.get_profile_photos("me", limit=1)))
+
         await event.client(functions.photos.UploadProfilePhotoRequest( file))
+
         os.system("rm -rf donottouch.jpg")
+        await event.edit("**Done !!! Check Your DP**")
+        await asyncio.sleep(3)
+        await event.delete()
         await asyncio.sleep(input1_Delay)
 CMD_HELP.update(
   {
