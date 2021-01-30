05fec706341fdc23d2c09824c9d0afced5be09f2
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-30 07:36:27 +0530

    Update antispam.py

--- userbot/plugins/antispam.py
+++ userbot/plugins/antispam.py
@@ -1,65 +1,200 @@
-"""
-credits to @mrconfused and @sandy1709
-"""
 #    Copyright (C) 2020  sandeep.n(π.$)
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from sqlalchemy import Column, String
+# baning spmmers plugin for catuserbot by @sandy1709 and @mrconfused
+# included both cas(combot antispam service) and spamwatch (need to add
+# more feaututres)
 
-from userbot.plugins.sql_helper import BASE, SESSION
+from requests import get
+from telethon import events
+from telethon.errors import ChatAdminRequiredError
+from telethon.tl.types import ChannelParticipantsAdmins
+
+from ..utils import admin_cmd, edit_or_reply
+from . import BOTLOG, BOTLOG_CHATID, CMD_HELP, LOGS, spamwatch
+from .sql_helper.gban_sql_helper import get_gbanuser
+
+if config.ANTISPAMBOT_BAN:
+
+    @borg.on(events.ChatAction())
+    async def anti_spambot(event):
+        if not event.user_joined and not event.user_added:
+            return
+        chat = event.chat_id
+        user = await event.get_user()
+        catadmin = await is_admin(bot, chat, bot.uid)
+        if not catadmin:
+            return
+        catbanned = None
+        adder = None
+        ignore = None
+        if event.user_added:
+            try:
+                adder = event.action_message.sender_id
+            except AttributeError:
+                return
+        async for admin in event.client.iter_participants(
+            event.chat_id, filter=ChannelParticipantsAdmins
+        ):
+            if admin.id == adder:
+                ignore = True
+                break
+        if ignore:
+            return
+        if is_gbanned(user.id):
+            catgban = get_gbanuser(user.id)
+            if catgban.reason:
+                hmm = await event.reply(
+                    f"[{user.first_name}](tg://user?id={user.id}) was gbanned by you for the reason `{catgban.reason}`"
+                )
+            else:
+                hmm = await event.reply(
+                    f"[{user.first_name}](tg://user?id={user.id}) was gbanned by you"
+                )
+            try:
+                await bot.edit_permissions(chat, user.id, view_messages=False)
+                catbanned = True
+            except Exception as e:
+                LOGS.info(e)
+        if spamwatch and not catbanned:
+            ban = spamwatch.get_ban(user.id)
+            if ban:
+                hmm = await event.reply(
+                    f"[{user.first_name}](tg://user?id={user.id}) was banned by spamwatch for the reason `{ban.reason}`"
+                )
+                try:
+                    await bot.edit_permissions(chat, user.id, view_messages=False)
+                    catbanned = True
+                except Exception as e:
+                    LOGS.info(e)
+        if not catbanned:
+            try:
+                casurl = "https://api.cas.chat/check?user_id={}".format(user.id)
+                data = get(casurl).json()
+            except Exception as e:
+                LOGS.info(e)
+                data = None
+            if data and data["ok"]:
+                reason = (
+                    f"[Banned by Combot Anti Spam](https://cas.chat/query?u={user.id})"
+                )
+                hmm = await event.reply(
+                    f"[{user.first_name}](tg://user?id={user.id}) was banned by Combat anti-spam service(CAS) for the reason check {reason}"
+                )
+                try:
+                    await bot.edit_permissions(chat, user.id, view_messages=False)
+                    catbanned = True
+                except Exception as e:
+                    LOGS.info(e)
+        if BOTLOG and catbanned:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                "#ANTISPAMBOT\n"
+                f"**User :** [{user.first_name}](tg://user?id={user.id})\n"
+                f"**Chat :** {event.chat.title} (`{event.chat_id}`)\n"
+                f"**Reason :** {hmm.text}",
+            )
 
 
-class GBan(BASE):
-    __tablename__ = "gban"
-    chat_id = Column(String(14), primary_key=True)
-    reason = Column(String(127))
-
-    def __init__(self, chat_id, reason=""):
-        self.chat_id = chat_id
-        self.reason = reason
-
-
-GBan.__table__.create(checkfirst=True)
-
-
-def is_gbanned(chat_id):
+@borg.on(admin_cmd(pattern="cascheck$"))
+async def caschecker(cas):
+    catevent = await edit_or_reply(
+        cas,
+        "`checking any cas(combot antispam service) banned users here, this may take several minutes too......`",
+    )
+    text = ""
+    chat = cas.chat_id
     try:
-        return SESSION.query(GBan).filter(GBan.chat_id == str(chat_id)).one()
-    except BaseException:
-        return None
-    finally:
-        SESSION.close()
-
-
-def get_gbanuser(chat_id):
+        info = await cas.client.get_entity(chat)
+    except (TypeError, ValueError) as err:
+        await cas.edit(str(err))
+        return
     try:
-        return SESSION.query(GBan).get(str(chat_id))
-    finally:
-        SESSION.close()
+        cas_count, members_count = (0,) * 2
+        banned_users = ""
+        async for user in cas.client.iter_participants(info.id):
+            if banchecker(user.id):
+                cas_count += 1
+                if not user.deleted:
+                    banned_users += f"{user.first_name}-`{user.id}`\n"
+                else:
+                    banned_users += f"Deleted Account `{user.id}`\n"
+            members_count += 1
+        text = "**Warning!** Found `{}` of `{}` users are CAS Banned:\n".format(
+            cas_count, members_count
+        )
+        text += banned_users
+        if not cas_count:
+            text = "No CAS Banned users found!"
+    except ChatAdminRequiredError as carerr:
+        await catevent.edit("`CAS check failed: Admin privileges are required`")
+        return
+    except BaseException as be:
+        await catevent.edit("`CAS check failed`")
+        return
+    await catevent.edit(text)
 
 
-def catgban(chat_id, reason):
-    adder = GBan(str(chat_id), str(reason))
-    SESSION.add(adder)
-    SESSION.commit()
+@borg.on(admin_cmd(pattern="spamcheck$"))
+async def caschecker(cas):
+    text = ""
+    chat = cas.chat_id
+    catevent = await edit_or_reply(
+        cas,
+        "`checking any spamwatch banned users here, this may take several minutes too......`",
+    )
+    try:
+        info = await cas.client.get_entity(chat)
+    except (TypeError, ValueError) as err:
+        await cas.edit(str(err))
+        return
+    try:
+        cas_count, members_count = (0,) * 2
+        banned_users = ""
+        async for user in cas.client.iter_participants(info.id):
+            if spamchecker(user.id):
+                cas_count += 1
+                if not user.deleted:
+                    banned_users += f"{user.first_name}-`{user.id}`\n"
+                else:
+                    banned_users += f"Deleted Account `{user.id}`\n"
+            members_count += 1
+        text = "**Warning! **Found `{}` of `{}` users are spamwatch Banned:\n".format(
+            cas_count, members_count
+        )
+        text += banned_users
+        if not cas_count:
+            text = "No spamwatch Banned users found!"
+    except ChatAdminRequiredError as carerr:
+        await catevent.edit("`spamwatch check failed: Admin privileges are required`")
+        return
+    except BaseException as be:
+        await catevent.edit("`spamwatch check failed`")
+        return
+    await catevent.edit(text)
 
 
-def catungban(chat_id):
-    rem = SESSION.query(GBan).get(str(chat_id))
-    if rem:
-        SESSION.delete(rem)
-        SESSION.commit()
+def banchecker(user_id):
+    try:
+        casurl = "https://api.cas.chat/check?user_id={}".format(user_id)
+        data = get(casurl).json()
+    except Exception as e:
+        LOGS.info(e)
+        data = None
+    return bool(data and data["ok"])
 
 
-def get_all_gbanned():
-    rem = SESSION.query(GBan).all()
-    SESSION.close()
-    return rem
+def spamchecker(user_id):
+    ban = None
+    if spamwatch:
+        ban = spamwatch.get_ban(user_id)
+    return bool(ban)
+
+
+CMD_HELP.update(
+    {
+        "antispambot": "**Plugin : **`antispambot`\
+        \n\n**Syntax : **`.cascheck`\
+        \n**Function : **__Searches for cas(combot antispam service) banned users in group and shows you the list__\
+        \n\n**Syntax : **`.spamcheck`\
+        \n**Function : **__Searches for spamwatch banned users in group and shows you the list__"
+    }
+)
