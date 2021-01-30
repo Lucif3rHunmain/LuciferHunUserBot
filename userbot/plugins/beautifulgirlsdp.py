7ee62df0d0cdd366b460ec70f75630c5f223f654
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-30 20:11:12 +0530

    Update beautifulgirlsdp.py

--- userbot/plugins/beautifulgirlsdp.py
+++ userbot/plugins/beautifulgirlsdp.py
@@ -150,6 +150,10 @@ async def main(event):
       input1_Delay = 300
 
     await event.edit("**Starting Beautiful Girls Profile Pics... **")
+    await asyncio.sleep(3)
+    await event.edit("**Started Check Your DP in Few Seconds**")
+    await asyncio.sleep(3)
+    await event.delete()
     while True:
 
         await girlspp()
@@ -159,9 +163,6 @@ async def main(event):
         await event.client(functions.photos.UploadProfilePhotoRequest( file))
 
         os.system("rm -rf donottouch.jpg")
-        await event.edit("**Done !!! Check Your DP**")
-        await asyncio.sleep(3)
-        await event.delete()
         await asyncio.sleep(input1_Delay)
         
 CMD_HELP.update(
