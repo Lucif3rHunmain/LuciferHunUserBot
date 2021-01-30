1ee45c8dec5dbaf5a056e568c7568ef8159ac90a
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-30 16:17:29 +0530

    Update actressdp.py

--- userbot/plugins/actressdp.py
+++ userbot/plugins/actressdp.py
@@ -59,17 +59,17 @@ async def main(event):
     if input_str == "":
      input1_Delay = 300
     else:
-    input_Delay = int(input_str.split(" ", 2)[0])
-    input_Time = str(input_str.split(" ", 2)[1])
-    if input_Time == 'd' or input_Time == 'D':
-     input1_Delay=(input_Delay*24*60*60)
-    elif input_Time == 'h' or input_Time == 'H':
-     input1_Delay=(input_Delay*60*60)
-    elif input_Time == 'm' or input_Time == 'M':
-     input1_Delay=(input_Delay*60)
-    elif input_Time == 's' or input_Time == 'S':
-     input1_Delay=input_Delay
-    else:
+     input_Delay = int(input_str.split(" ", 2)[0])
+     input_Time = str(input_str.split(" ", 2)[1])
+     if input_Time == 'd' or input_Time == 'D':
+      input1_Delay=(input_Delay*24*60*60)
+     elif input_Time == 'h' or input_Time == 'H':
+      input1_Delay=(input_Delay*60*60)
+     elif input_Time == 'm' or input_Time == 'M':
+      input1_Delay=(input_Delay*60)
+     elif input_Time == 's' or input_Time == 'S':
+      input1_Delay=input_Delay
+     else:
       input1_Delay = 300
 
     await event.edit("**Starting Actress Profile Pics... **")
