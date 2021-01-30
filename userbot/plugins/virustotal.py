d9117bf6a9fa6030a3430610bb5634d1295fbb54
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-30 03:18:04 +0000

    Create virustotal.py

---
+++ userbot/plugins/virustotal.py
@@ -0,0 +1,35 @@
+# ported to dc by @Lucif3rHun
+import asyncio
+import os
+import requests
+from userbot import CMD_HELP
+from userbot.utils import edit_or_reply, edit_delete, admin_cmd
+from userbot.uniborgConfig import Config
+
+VIRUSTOTAL_API_KEY = os.environ.get("VIRUSTOTAL_API_KEY", None)
+@borg.on(admin_cmd(pattern="detect$", outgoing=True))
+async def detect(event):
+    if Config.DEEP_AI is None:
+        return await edit_delete(
+            event, "Add VAR `` and get API get from http://www.virustotal.com", 5
+        )
+    reply = await event.get_reply_message()
+    if not reply:
+        return await edit_delete(
+            event, "`Reply to a media file!`", 5
+        )
+    catevent = await edit_or_reply(event, "`Downloading the file to check...`")
+    media = await event.client.download_media(reply)
+    catevent = await edit_or_reply(event, "`Scanning the file for any kind of virus or malware and abnormal behaviour`")
+    url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+    params = {'apikey': VIRUSTOTAL_API_KEY}
+    files = {'file': open(media, 'rb'))}
+    response = requests.post(url, files=files, params=params)
+    print(response.json())
+    os.remove(media)
+CMD_HELP.update(
+        {
+        "VirusTotal": ".vtscan"
+                      "\nUsage: Reply any media .vtscan command to any media file to scan it for malware or virus and any malicious behaviour. \n\n"
+    }
+)
