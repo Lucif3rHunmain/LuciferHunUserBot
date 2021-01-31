fc2a84e7dffc610fa69ed27920d5e2f25239a9c1
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-31 15:48:32 +0530

    Update faketempmail.py

--- userbot/plugins/faketempmail.py
+++ userbot/plugins/faketempmail.py
@@ -28,10 +28,36 @@ async def _(event):
           else: 
              await event.edit(f"{response.message.message}")
 
+@borg.on(admin_cmd("ftmaills ?(.*)"))
+async def _(event):
+
+    chat = "@fakemailbot"
+    await event.edit("```Getting you a fakemail My Master```")
+    async with borg.conversation(chat) as conv:
+          try:
+              await conv.send_message('/id')
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=177914997))
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @fakemailbot and try again```")
+              return
+          if response.text.startswith("send"):
+             await event.edit("```can you kindly disable your forward privacy settings for good?```")
+          elif response.text.startswith("An error occured")
+             await conv.send_message('/id')
+             response = conv.wait_event(events.NewMessage(incoming=True,from_users=177914997))
+             response = await response
+             await event.edit(f"{response.message.message}")
+          else: 
+             await event.edit(f"{response.message.message}")
+
 CMD_HELP.update(
     {
         "Fakemail": "__**PLUGIN NAME :** Tempmail__\
-    \n\n📌** CMD ★** `.tmail`\
-    \n**USAGE   ★  **Get yourself a Tempmail without going to web"
+    \n\n📌** CMD ★** `.ftmail`\
+    \n**USAGE   ★  **Get yourself a Tempmail without going to web
+    \n\n📌** CMD ★** `.ftmaills`\
+    \n**USAGE   ★  **Gets you the list of fake temp mails you generated."
+
 }
 )
