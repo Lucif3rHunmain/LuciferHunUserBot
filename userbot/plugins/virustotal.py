ad2ae2609b77acd237ec1679088aa9539c8b2109
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-31 11:08:10 +0530

    Update virustotal.py

--- userbot/plugins/virustotal.py
+++ userbot/plugins/virustotal.py
@@ -22,30 +22,12 @@ async def vtscan(event):
         )
     catevent = await edit_or_reply(event, "`Downloading the file to check...`")
     media = await event.client.download_media(reply)
-    catevent = await edit_or_reply(event, "`Scanning the file for any kind of virus or malware and abnormal behaviour. Please Wait...`")
-    scanurl = 'https://www.virustotal.com/vtapi/v2/file/scan'
-    params = {'apikey': VIRUSTOTAL_API_KEY}
-    files = {'file': open(media, 'rb')}
-    vresponse = requests.post(scanurl, files=files, params=params)
-    os.remove(media)
-    catevent = await edit_or_reply(event, vresponse.json())
-    msg = vresponse.json()["vebrose_msg"]
-    if msg.text.startswith("Scan rquest successfully queued"):
-     catevent = await edit_or_reply(event, "File successfully uploaded for scanning. Wait for 1 mintues to get the scan results")
-     await asyncio.sleep(10)
-     catevent = await edit_or_reply(event, "File Scan Initialized")
-     await asyncio.sleep(10)
-     md5 = vresponse.json()["md5"]
-     sha1 = vresponse.json()["sha1"]
-     sha256 = vresponse.json()["sha256"]
-     resource = vresponse.json()["resource"]
-     reporturl = 'https://www.virustotal.com/vtapi/v2/file/report'
-     params = {'apikey': Config.VIRUSTOTAL_API_KEY, 'resource': resource}
-     response1 = requests.get(reporturl, params=params)
-     catevent = await edit_or_reply(event, f,"File Hash \n Md5- {md5} \n Sha1- {sha1} \n Sha256- {sha256}")
-     catevent = await edit_or_reply(event, response1.json())
-    else:
-     catevent = await edit_or_reply(event, vresponse.json())
+    url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+    params = {'apikey': 'Config.VIRUSTOTAL_API_KEY'}
+    files = {'file': open(media, 'rb'))}
+    response = requests.post(url, files=files, params=params)
+    await event.edit(response.json())
+
 CMD_HELP.update(
         {
         "VirusTotal": ".vtscan"
