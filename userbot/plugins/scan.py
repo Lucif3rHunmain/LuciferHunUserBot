3293c94bd1f5dc3b5a99e25e98cafb33108ff4e5
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-02-06 20:05:23 +0530

    Update scan.py

--- userbot/plugins/scan.py
+++ userbot/plugins/scan.py
@@ -1,3 +1,7 @@
+# -0,0 +1,96 @@
+# Made For DARK COBRA and TELEBOT...
+# Made by team cobra with @xditya
+# Retrieves the name history and the username history of the replied user..
 import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -5,7 +9,7 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.utils import admin_cmd
 from userbot import bot, CMD_HELP
 
-@borg.on(admin_cmd(pattern="scan ?(.*)"))
+@borg.on(admin_cmd(pattern=r"scan"))
 async def _(event):
     if event.fwd_from:
         return 
@@ -13,7 +17,7 @@ async def _(event):
        await event.edit("Reply to any user message.")
        return
     reply_message = await event.get_reply_message() 
-    chat = "@tgscanrobot"
+    chat = "Sangmatainfo_bot"
     sender = reply_message.sender.id
     if reply_message.sender.bot:
        await event.edit("Reply to actual users message.")
@@ -23,16 +27,19 @@ async def _(event):
           try:     
               #await conv.send_message("/search_id {}".format(sender))
               response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=1557162396))
-              await conv.send_message('{}'.format(sender))
-              response1 = await response1
-          except YouBlockedUserError:
-              await event.reply("Please unblock bot ")
+              response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=1557162396))
+              response3 = conv.wait_event(events.NewMessage(incoming=True,from_users=1557162396))
+              await conv.send_message("/search_id {}".format(sender))
+              response1 = await response1 
+              response2 = await response2 
+              response3 = await response3 
+          except YouBlockedUserError: 
+              await event.reply("Please unblock ( @tgscanrobot ) ")
               return
-          if response1.text.startswith("No records found"):
-             await event.edit("User never changed his Username...")
-          else: 
              await event.delete()
-             await event.client.send_message(event.chat_id, response1.message)
+             await event.client.send_message(event.chat_id, response2.message)
+             
+             await event.client.send_message(event.chat_id, response3.message)
 
 CMD_HELP.update(
     {
