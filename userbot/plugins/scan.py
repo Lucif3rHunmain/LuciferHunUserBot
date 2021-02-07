89df989f37c90a6c2fdbed94129bac1711d4ff8a
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-02-07 15:11:47 +0530

    Update scan.py

--- userbot/plugins/scan.py
+++ userbot/plugins/scan.py
@@ -5,7 +5,7 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.utils import admin_cmd
 from userbot import bot, CMD_HELP
 
-@borg.on(admin_cmd(pattern=r"scan"))
+@borg.on(admin_cmd(pattern="scen ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return 
@@ -13,32 +13,31 @@ async def _(event):
        await event.edit("Reply to any user message.")
        return
     reply_message = await event.get_reply_message() 
-    chat = "tgscanrobot"
+    chat = "@tgscanrobot"
     sender = reply_message.sender.id
     if reply_message.sender.bot:
        await event.edit("Reply to actual users message.")
        return
     await event.edit("Checking...")
     async with event.client.conversation(chat) as conv:
-          try:
+          try:     
+              #await conv.send_message("/search_id {}".format(sender))
               response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=1557162396))
-              response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=1557162396))
-              response3 = conv.wait_event(events.NewMessage(incoming=True,from_users=1557162396))
               await conv.send_message('{}'.format(sender))
-              response1 = await response1 
-              response2 = await response2 
-              response3 = await response3 
-          except YouBlockedUserError: 
-              await event.reply("Please unblock ( @tgscanrobot ) ")
+              response1 = await response1
+          except YouBlockedUserError:
+              await event.reply("Please unblock bot ")
               return
-              await event.delete()
-              await event.client.send_message(event.chat_id, response2.message)
-              await event.client.send_message(event.chat_id, response3.message)
+          if response1.text.startswith("This human is not in my database."):
+             await event.edit("This human is not in my database. It a new account and hasn't joined to ant chat.")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response1.message)
 
 CMD_HELP.update(
     {
         "ScanGroup": "__**PLUGIN NAME :** ScanGroup__\
-    \n\nðŸ“Œ** CMD â˜…** `.scan`\
+    \n\nðŸ“Œ** CMD â˜…** `.sg`\
     \n**USAGE   â˜…  **Retrieves the Number and Names of the Groups the User Joined"
     }
 )
