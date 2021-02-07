ed3084b9334d7f1ff6821827abb224e07624938e
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-02-07 15:23:31 +0530

    Update cricket.py

--- userbot/plugins/cricket.py
+++ userbot/plugins/cricket.py
@@ -8,56 +8,81 @@ from userbot.utils import admin_cmd, edit_or_reply
 from userbot import CMD_HELP
 
 
-@borg.on(admin_cmd(pattern=r"cric (.*)"))
+@borg.on(admin_cmd(pattern=r"cricket score"))
 async def _(event):
-    input_str = "".join(event.text.split(maxsplit=1)[1:])
-    if input_str == " ":
-     await event.edit("Please give a correct command. Type `.help cricket` to know more")
-     await asyncio.sleep(5)
-     await event.delete
-    else:
-     input_cmd = str(input_str.split(" ", 2)[0])
-     if input_cmd == 'score' or input_cmd == 'Score':
-      if event.fwd_from:
+    if event.fwd_from:
         return
-        chat = "@cricbuzz_bot"
-        reply_to_id = event.message
-        lucievent = await edit_or_reply(event, "Gathering info...")
-        async with event.client.conversation(chat) as conv:
-         try:
-            msg_start = await conv.send_message('/score')
+    chat = "@cricbuzz_bot"
+    reply_to_id = event.message
+    megaevent = await edit_or_reply(event, "Gathering info...")
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            response = await conv.get_response()
+            msg = await conv.send_message("/score")
             respond = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
-         except YouBlockedUserError:
+        except YouBlockedUserError:
             await megaevent.edit("Unblock @cricbuzz_bot & try again")
             return
-         if respond.text.startswith("I can't find that"):
-            await lucievent.edit("sorry I can't find it")
-         else:
-            await lucievent.delete()
+        if respond.text.startswith("I can't find that"):
+            await megaevent.edit("sorry i can't find it")
+        else:
+            await megaevent.delete()
             await event.client.send_message(
                 event.chat_id, respond.message, reply_to=reply_to_id
             )
-         await event.client.delete_messages(
+        await event.client.delete_messages(
             conv.chat_id, [msg_start.id, msg.id, response.id, respond.id]
         )
-     elif input_cmd == 'recent' or input_cmd == 'Recent':
-      if event.fwd_from:
+     
+@borg.on(admin_cmd(pattern=r"cricket recent"))
+async def _(event):
+    if event.fwd_from:
         return
-        chat = "@cricbuzz_bot"
-        reply_to_id = event.message
-        lucievent = await edit_or_reply(event, "Gathering info...")
-        async with event.client.conversation(chat) as conv:
-         try:
-            msg_start = await conv.send_message('/recent')
+    chat = "@cricbuzz_bot"
+    reply_to_id = event.message
+    megaevent = await edit_or_reply(event, "Gathering info...")
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            response = await conv.get_response()
+            msg = await conv.send_message("/recent")
             respond = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
-         except YouBlockedUserError:
-            await lucievent.edit("Unblock @cricbuzz_bot & try again")
+        except YouBlockedUserError:
+            await megaevent.edit("Unblock @cricbuzz_bot & try again")
             return
-         if respond.text.startswith("I can't find that"):
-            await lucivent.edit("sorry i can't find it")
-         else:
+        if respond.text.startswith("I can't find that"):
+            await megaevent.edit("sorry i can't find it")
+        else:
+            await megaevent.delete()
+            await event.client.send_message(
+                event.chat_id, respond.message, reply_to=reply_to_id
+            )
+        await event.client.delete_messages(
+            conv.chat_id, [msg_start.id, msg.id, response.id, respond.id]
+        )
+@borg.on(admin_cmd(pattern=r"cricket upcoming"))
+async def _(event):
+    if event.fwd_from:
+        return
+    chat = "@cricbuzz_bot"
+    reply_to_id = event.message
+    megaevent = await edit_or_reply(event, "Gathering info...")
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message("/start")
+            response = await conv.get_response()
+            msg = await conv.send_message("/score")
+            respond = await conv.get_response()
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await megaevent.edit("Unblock @cricbuzz_bot & try again")
+            return
+        if respond.text.startswith("I can't find that"):
+            await megaevent.edit("sorry i can't find it")
+        else:
             await megaevent.delete()
             await event.client.send_message(
                 event.chat_id, respond.message, reply_to=reply_to_id
@@ -66,46 +91,28 @@ async def _(event):
             conv.chat_id, [msg_start.id, msg.id, response.id, respond.id]
         )
 
-     elif input_cmd == 'upcoming' or input_cmd == 'Upcoming':
-       if event.fwd_from:
+@borg.on(admin_cmd(pattern=r"cricket (.*)"))
+async def _(event):
+    if event.fwd_from:
         return
-        chat = "@cricbuzz_bot"
-        reply_to_id = event.message
-        lucivent = await edit_or_reply(event, "Gathering info...")
-        async with event.client.conversation(chat) as conv:
-         try:
-            msg_start = await conv.send_message('/upcoming')
-            respond = await conv.get_response()
-            await event.client.send_read_acknowledge(conv.chat_id)
-         except YouBlockedUserError:
-            await megaevent.edit("Unblock @cricbuzz_bot & try again")
-            return
-         if respond.text.startswith("I can't find that"):
-            await lucievent.edit("sorry i can't find it")
-         else:
-            await lucievent.delete()
-            await event.client.send_message(
-                event.chat_id, respond.message, reply_to=reply_to_id
-            )
-         await event.client.delete_messages(
-            conv.chat_id, [msg_start.id, msg.id, response.id, respond.id]
-        )
-     else:
-      chat = "@cricbuzz_bot"
-      reply_to_id = event.message
-      lucievent = await edit_or_reply(event, "Collecting information...")
-      async with event.client.conversation(chat) as conv:
+    details = event.pattern_match.group(1)
+    chat = "@cricbuzz_bot"
+    reply_to_id = event.message
+    megaevent = await edit_or_reply(event, "Collecting information...")
+    async with event.client.conversation(chat) as conv:
         try:
-            msg_start = await conv.send_message(f"{input_cmd}")
+            msg_start = await conv.send_message("/start")
+            response = await conv.get_response()
+            msg = await conv.send_message(f"{details}")
             respond = await conv.get_response()
             await event.client.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await lucievent.edit("Unblock @cricbuzz_bot & try again")
+            await megaevent.edit("Unblock @cricbuzz_bot & try again")
             return
         if respond.text.startswith("I can't find that"):
-            await lucievent.edit("sorry i can't find it")
+            await megaevent.edit("sorry i can't find it")
         else:
-            await lucievent.delete()
+            await megaevent.delete()
             await event.client.send_message(
                 event.chat_id, respond.message, reply_to=reply_to_id
             )
@@ -116,13 +123,13 @@ async def _(event):
 CMD_HELP.update(
     {
         "cricket": "**Plugin :** cricket\
-      \n\n**  • Syntax : **.cric <score> \
+      \n\n**  • Syntax : **.cricket score \
       \n**  • Function : **__To see score of ongoing matches.__\
-      \n\n**  • Syntax : **.cric <recent>\
+      \n\n**  • Syntax : **.cricket recent\
       \n**  • Function : **__Recent Match Result.__\
-      \n\n**  • Syntax : **.cric <upcoming>\
+      \n\n**  • Syntax : **.cricket upcoming\
       \n**  • Function : **__To get results of upcoming match.__\
-      \n\n**  • Syntax : **.cric <cmd>\
+      \n\n**  • Syntax : **.cricket <cmd>\
       \n**  • Function : **__To get details.__\
       \n\n**  • Example :-** .cric /score...."
     }
