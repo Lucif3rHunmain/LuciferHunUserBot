dadaa3b748512ecd31a924f5e1811229ba9c7541
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-02-08 15:16:10 +0530

    Update lastfm.py

--- userbot/plugins/lastfm.py
+++ userbot/plugins/lastfm.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 
 from asyncio import sleep
-from os import environ
+import os
 from re import sub
 from sys import setrecursionlimit
 from urllib import parse
