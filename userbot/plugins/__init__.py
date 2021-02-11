b7f64265251b37bb3d3e0e59a10619d1608f7237
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-02-11 06:22:39 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,5 +1,17 @@
 from userbot import darkdef
 from ..helper import functions as dcdef
+import lottie
 #USELESS
 darkmusic = darkdef.darkmusic 
 darkmusicvideo = darkdef.darkmusicvideo
+
+async def make_gif(event, reply, quality=None, fps=None):
+    fps = fps or 1
+    quality = quality or 256
+    result_p = os.path.join("temp", "animation.gif")
+    animation = lottie.parsers.tgs.parse_tgs(reply)
+    with open(result_p, "wb") as result:
+        await _catutils.run_sync(
+            lottie.exporters.gif.export_gif, animation, result, quality, fps
+        )
+    return result_p
